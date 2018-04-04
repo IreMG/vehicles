3e5e9605d24649a96305a237fc02f38d03f3db47
ire <ire@Ire>
2018-04-04 14:24:59 +0200

    Ordenar

--- Vehicles/src/Vehicle.java
+++ Vehicles/src/Vehicle.java
@@ -1,15 +1,16 @@
 
 public class Vehicle {
+	private int cilindrada;
+	private int portes;
+	private int velocidad_actual;
+	private static final int VELOCIDAD_MAXIMA=120;
    public Vehicle(int cilindrada, int portes, int velocidad_actual) {
 		this.cilindrada = cilindrada;
 		this.portes = portes;
 		this.velocidad_actual = 0;
 	}
 
-private int cilindrada;
-   private int portes;
-   private int velocidad_actual;
-   private static final int VELOCIDAD_MAXIMA=120;
+
    
    public void acelerar() {
 	   if(velocidad_actual<VELOCIDAD_MAXIMA) {
